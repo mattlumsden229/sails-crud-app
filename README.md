e7d39f8c3cfc14a26a380f4fafc878ecd601a014
mattlumsden229 <121324815+mattlumsden229@users.noreply.github.com>
2023-02-01 11:58:45 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1>Simple CRUD app using Sails.js</h1>
+
+<h2>Usage</h2>
+
+<h3>Install the dependencies</h3>
+
+<p>$ npm install sails -g</p>
+
+<p>$ npm install</p>
+
+<h3>Server</h3>
+
+<p>$ sails lift</p>
---
+++ config/routes.js
@@ -0,0 +1,49 @@
+/**
+ * Route Mappings
+ * (sails.config.routes)
+ *
+ * Your routes tell Sails what to do each time it receives a request.
+ *
+ * For more information on configuring custom routes, check out:
+ * https://sailsjs.com/anatomy/config/routes-js
+ */
+
+module.exports.routes = {
+
+  /***************************************************************************
+  *                                                                          *
+  * Make the view located at `views/homepage.ejs` your home page.            *
+  *                                                                          *
+  * (Alternatively, remove this and add an `index.html` file in your         *
+  * `assets` directory)                                                      *
+  *                                                                          *
+  ***************************************************************************/
+
+  '/': { view: 'pages/homepage' },
+  '/articles/list': {
+    controller: 'ArticlesController',
+    action: 'list' // "list" was the name of the function in the video, yours could differ
+    },
+  '/articles/add': {
+    controller: 'ArticlesController',
+    action: 'add'
+    },
+    '/articles/create': 'ArticlesController.create',
+    '/articles/delete/:id?': 'ArticlesController.delete',
+    '/articles/edit/:id?': 'ArticlesController.edit',
+    '/articles/update/:id?': 'ArticlesController.update',
+
+
+  /***************************************************************************
+  *                                                                          *
+  * More custom routes here...                                               *
+  * (See https://sailsjs.com/config/routes for examples.)                    *
+  *                                                                          *
+  * If a request to a URL doesn't match any of the routes in this file, it   *
+  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
+  * not match any of those, it is matched against static assets.             *
+  *                                                                          *
+  ***************************************************************************/
+
+
+};
