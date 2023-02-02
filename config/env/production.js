22b46011661f96d6db97db4db75bc7ac53a6512c
mattlumsden229 <121324815+mattlumsden229@users.noreply.github.com>
2023-02-02 15:47:10 -0600

    update

--- config/env/production.js
+++ config/env/production.js
@@ -47,6 +47,7 @@ module.exports = {
     ***************************************************************************/
     default: {
       adapter: 'sails-mongo',
+      url: 'mongodb+srv://article:base@cluster0.eoarpem.mongodb.net/test'
       //--------------------------------------------------------------------------
       //  /\   To avoid checking it in to version control, you might opt to set
       //  ||   sensitive credentials like `url` using an environment variable.
